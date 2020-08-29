a9d06479de5bc4816196f88910a292b648a06c13
Najmul-Islam <najmulislam519@gmail.com>
2020-08-29 17:06:18 +0600

    color changer site

---
+++ 01 ChangeBgColor/script.js
@@ -0,0 +1,41 @@
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = [
+	'AliceBlue',
+	'AntiqueWhite',
+	'Aqua',
+	'Aquamarine',
+	'DimGrey',
+	'DeepSkyBlue',
+	'DeepSkyBlue',
+	'DeepPink',
+	'DarkViolet',
+	'DarkTurquoise',
+	'DarkSlateGrey',
+	'DarkSlateBlue',
+	'DarkSeaGreen',
+	'DarkSalmon',
+	'DarkRed',
+	'DarkRed',
+	'DarkOrchid',
+	'DarkOrange',
+	'DarkOliveGreen',
+	'DarkMagenta',
+	'Cornsilk',
+	'Crimson',
+	'Cyan',
+	'DarkBlue',
+	'DarkCyan',
+	'DarkGoldenRod',
+	'DarkGray',
+	'DarkGreen',
+	'Gold',
+	'Gray',
+	'Green',
+];
+
+body.style.backgroundColor = 'red';
+button.addEventListener('click', function () {
+	const indexNo = parseInt(Math.random() * colors.length);
+	body.style.backgroundColor = colors[indexNo];
+})
